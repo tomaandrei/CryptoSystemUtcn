d58955332ae10fc740fa5184a144fa6ab22cee60
Florentinta Cociota <Florentinta Cociota>
2016-06-11 21:29:43 +0300

    Add ReceiveImage methods and change the save image details methode

--- CryptoSystemDissertation/Controllers/ImageBoardController.cs
+++ CryptoSystemDissertation/Controllers/ImageBoardController.cs
@@ -12,6 +12,7 @@ namespace CryptoSystemDissertation.Controllers
 {
     public class ImageBoardController : Controller
     {
+        private ImageDetails imageDetails;
         // GET: ImageBoard
         public ActionResult Index()
         {
@@ -25,22 +26,22 @@ namespace CryptoSystemDissertation.Controllers
 
         [HttpPost]
         public ActionResult ParametersImage(Sender senderJson)
-        {                     
+        {
             var crtUser = SessionManager.ReturnSessionObject("User") as UserAccount;
             if (ModelState.IsValid)
             {
-                    if (crtUser != null)
-                    {
-                        var imageDetails = SetImageDetails(crtUser.UserID.ToString(), senderJson.ReceiverId);                      
-                        var encryptParameters = new EncryptParameters<Parameters>(senderJson.RSAKeyXML, imageDetails.Parameters);
-                        var parameters = encryptParameters.Encrypt();
+                if (crtUser != null)
+                {
+                    SetImageDetails(crtUser.UserID.ToString(), senderJson.ReceiverId);
+                    var encryptParameters = new EncryptParameters<Parameters>(senderJson.RSAKeyXML, this.imageDetails.Parameters);
+                    var parameters = encryptParameters.Encrypt();
 
-                        return Json(new { encryptParam = parameters });
-                    }
-                    else
-                    {
-                        return RedirectToAction("Login");
-                    }             
+                    return Json(new { encryptParam = parameters });
+                }
+                else
+                {
+                    return RedirectToAction("Login");
+                }
             }
             return View();
         }
@@ -55,10 +56,10 @@ namespace CryptoSystemDissertation.Controllers
                 {
                     using (CryptoDbContext db = new CryptoDbContext())
                     {
-                        var sender = db.ImageDetails.Where(u => u.SenderId == crtUser.UserID.ToString() && u.ReceiverId== senderJson.ReceiverId).FirstOrDefault();
-                        if (sender != null)
+                        if (senderJson.ReceiverId != imageDetails.ReceiverId)
                         {
-                            sender.Image = senderJson.Image;
+                            this.imageDetails.Image = senderJson.Image;
+                            db.ImageDetails.Add(this.imageDetails);
                             db.SaveChanges();
                             ViewBag.Message = "The image was successfully add in Database!";
                         }
@@ -72,25 +73,74 @@ namespace CryptoSystemDissertation.Controllers
             return View();
         }
 
-        private ImageDetails SetImageDetails(string senderId, string receiverId)
+        private void SetImageDetails(string senderId, string receiverId)
         {
-            var imageDetails = new ImageDetails();
-            using (CryptoDbContext db = new CryptoDbContext())
+            this.imageDetails = new ImageDetails();
+
+            var randome = new RandomParameters();
+            imageDetails.SenderId = senderId;
+            imageDetails.ReceiverId = receiverId;
+            imageDetails.Parameters = new Parameters
             {
-                var randome = new RandomParameters();
-                imageDetails.SenderId = senderId;
-                imageDetails.ReceiverId = receiverId;
-                imageDetails.Parameters = new Parameters
-                {
-                    ParamtersId = int.Parse(senderId),
-                    Lambda = randome.GenerateLambdaRandomNumber(),
-                    X = randome.GenerateXRandomNumber()
-                };
-                imageDetails.Image = null;
-                db.ImageDetails.Add(imageDetails);
-                db.SaveChanges();
-            }
-            return imageDetails;
+                ParamtersId = int.Parse(senderId),
+                Lambda = randome.GenerateLambdaRandomNumber(),
+                X = randome.GenerateXRandomNumber()
+            };
+            imageDetails.Image = null;
         }
+
+        [HttpPost]
+        public ActionResult ReceiveImage()
+        {
+            var crtUser = SessionManager.ReturnSessionObject("User") as UserAccount;
+            if (crtUser != null)
+            {
+                using (CryptoDbContext db = new CryptoDbContext())
+                {
+                    var receive = db.ImageDetails.Where(r => r.ReceiverId == crtUser.UserID.ToString()).FirstOrDefault();
+                    if (receive != null)
+                    {
+                        return Json(new { message = "You have an image" });
+                    }
+                    else
+                    {
+                        return Json(new { message = "No image" });
+                    }
+                }
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ReceiveImage(Sender sender)
+        {
+            var crtUser = SessionManager.ReturnSessionObject("User") as UserAccount;
+            if (crtUser != null)
+            {
+                using (CryptoDbContext db = new CryptoDbContext())
+                {
+                    var receive = db.ImageDetails.Where(r => r.ReceiverId == crtUser.UserID.ToString()).FirstOrDefault();
+                    if(receive!=null)
+                    {
+                        var encryptParameters = new EncryptParameters<Parameters>(sender.RSAKeyXML, receive.Parameters);
+                        var parameters = encryptParameters.Encrypt();
+                        db.ImageDetails.Remove(receive);
+                        db.SaveChanges();
+                        return Json(new { Parameters = parameters, EncryptImage = receive.Image });
+                    }
+                    else
+                    {
+                        return Json(new { Message = "No image" });
+                    }
+                }
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        } 
     }
 }
