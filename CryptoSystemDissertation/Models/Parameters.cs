6034690dd636f12f38cfc4893cfa36fd0c40fda1
Florentinta Cociota <Florentinta Cociota>
2016-07-06 18:32:03 +0300

    Change parameters serialization

--- CryptoSystemDissertation/Models/Parameters.cs
+++ CryptoSystemDissertation/Models/Parameters.cs
@@ -1,12 +1,20 @@
-﻿namespace CryptoSystemDissertation.Models
+﻿using System.Xml.Serialization;
+
+namespace CryptoSystemDissertation.Models
 {
     public class Parameters
     {
+        [XmlAttribute]
         public double Lambda { get; set; }
+        [XmlAttribute]
         public double X { get; set; }
+        [XmlAttribute]
         public int T { get; set; }
+        [XmlAttribute]
         public int A { get; set; }
+        [XmlAttribute]
         public int B { get; set; }
+        [XmlAttribute]
         public int C0 { get; set; }
                   
     }
