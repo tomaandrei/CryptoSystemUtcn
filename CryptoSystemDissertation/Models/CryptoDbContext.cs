e82b6566ec00184600af00c4fe9368b57f146d05
tomaandrei <tomaandreiandrei@gmail.com>
2016-07-18 00:52:07 -0700

    db changes / configs for App Harbor

--- CryptoSystemDissertation/Models/CryptoDbContext.cs
+++ CryptoSystemDissertation/Models/CryptoDbContext.cs
@@ -4,6 +4,12 @@ namespace CryptoSystemDissertation.Models
 {
     public class CryptoDbContext : DbContext
     {
+      protected override void OnModelCreating(DbModelBuilder modelBuilder)
+      {
+        Database.SetInitializer<CryptoDbContext>(new CreateDatabaseIfNotExists<CryptoDbContext>());
+        base.OnModelCreating(modelBuilder);
+      }
+
         public DbSet<UserAccount> UserAccount { get; set; }
         public DbSet<ImageDetails> ImageDetails { get; set; }
     }
